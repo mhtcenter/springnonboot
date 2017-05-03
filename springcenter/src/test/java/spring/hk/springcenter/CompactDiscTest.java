0fc1642b782cefda6cd46d1528f2f144fc440f3a
HuangMarco <marco.huang@sap.com>
2017-05-03 17:27:12 +0800

    Updated the application

---
+++ springcenter/src/test/java/spring/hk/springcenter/CompactDiscTest.java
@@ -0,0 +1,28 @@
+package spring.hk.springcenter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import spring.hk.springcenter.component.CompactDisc;
+
+/**
+ * @author I323691
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=SpringCenterApplication.class)
+public class CompactDiscTest {
+	
+	@Autowired
+	private CompactDisc dc;
+	
+	@Test
+	public void dcShouldNotNull(){
+		assertNotNull(dc);
+	}
+}
