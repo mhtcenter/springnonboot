91cbbefe2c978e385d1f2b5e68e367cac4338d93
HuangMarco <marco.huang@sap.com>
2017-05-03 17:19:51 +0800

    Updated the config file

--- springcenter/src/main/java/spring/hk/springcenter/component/config/CompactDiscConfig.java
+++ springcenter/src/main/java/spring/hk/springcenter/component/config/CompactDiscConfig.java
@@ -1,12 +1,14 @@
-package spring.hk.springcenter;
+package spring.hk.springcenter.component.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author I323691
+ *
+ */
 @Configuration
 @ComponentScan
-public class SpringCenterApplication
-{
-    public static void main(String[] args) {
-    }
+public class CompactDiscConfig {
+
 }
